fcb23f9871887d880b4fe93e3f35982f0c37de17
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-03-11 20:20:08 -0700

    Update Shortest-steps-to-a-number.js

--- Shortest-steps-to-a-number.js
+++ Shortest-steps-to-a-number.js
@@ -45,3 +45,20 @@ function shortestStepsToNum(num) {
   }
   return counter;
 }
+
+// or
+
+function shortestStepsToNum(num) {
+  let steps = 0;
+  let n = num;
+  while (n > 1) {
+    if (n % 2 !== 0) {
+        n = n - 1;
+        steps++;
+    } else {
+      n = n / 2;
+      steps++;
+     }
+  }
+  return steps
+}
